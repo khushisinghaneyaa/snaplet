2ec7df26c7b0bed8b64536c6b728dbeea7dfa7ff
khushisinghaneyaa <khushisighaneyaa@gmail.com>
2026-05-30 22:57:19 +0530

    first commit

---
+++ Frontend/src/app/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+
+import React from 'react'
+
+function App  ()  {
+  return (
+    <>
+    <h1>hello world</h1>
+    </>
+  )
+}
+
+export default App
+
+
